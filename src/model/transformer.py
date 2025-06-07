80db90fe20984341aeb05c2aef035f18dc5dbeb7
Joshua Chung <joshua1b2823@gmail.com>
2025-06-07 23:52:32 +0000

    transformer.py: replace deprecated `.byte()` with `.bool()`
    pytorch deprecated byte mask input
    
    Closes #4

--- src/model/transformer.py
+++ src/model/transformer.py
@@ -711,7 +711,7 @@ class TransformerModel(nn.Module):
 
         # add <EOS> to unfinished sentences
         if cur_len == max_len:
-            generated[-1].masked_fill_(unfinished_sents.byte(), self.eos_index)
+            generated[-1].masked_fill_(unfinished_sents.bool(), self.eos_index)
 
         # sanity check
         assert (generated == self.eos_index).sum() == 2 * bs
